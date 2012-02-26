ab176bc69a6562df2424212cfb0f2d5ad23f06d9
Peter J. Weisberg <pj@irregularexpressions.net>
2012-02-26 12:31:00 -0800

    Make cast photos alternate between being on the right and on the left

--- bios/include-bios.php
+++ bios/include-bios.php
@@ -3,10 +3,21 @@
 $bio_template = file_get_contents("bios/template.html");
 
 foreach (glob("bios/*.bio") as $filename) {
+  switch_photo_side();
   process($filename, $bio_template);
 }
 
+function switch_photo_side(){
+  global $photo_side;
+  if( $photo_side == "right" ){
+    $photo_side = "left";
+  }else{
+    $photo_side = "right";
+  }
+}
+
 function process($filename, $template){
+  global $photo_side;
   preg_match('@^(?:bios/\d+-)?(.*).bio@i',
              $filename, $matches);
   $identifier = $matches[1];
@@ -16,8 +27,8 @@ function process($filename, $template){
   $cast_photo = trim(fgets($file));
   $text = fread($file, 8192);
 
-  $placeholders = array( "@@id@@",    "@@name@@", "@@photo@@", "@@text@@" );
-  $replacements = array( $identifier, $cast_name, $cast_photo, $text );
+  $placeholders = array( "@@id@@",    "@@name@@", "@@photo@@", "@@text@@", "@@photo-side@@" );
+  $replacements = array( $identifier, $cast_name, $cast_photo, $text, $photo_side );
 
   echo str_replace( $placeholders, $replacements, $template );
 }
