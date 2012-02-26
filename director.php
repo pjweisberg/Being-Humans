793cf8881bc781e1161ca99bbe539fbca47f5880
Peter J. Weisberg <pj@irregularexpressions.net>
2012-02-26 11:35:04 -0800

    Inline stan-sidebar.php
    There's no longer a separate page for Stan's class, so it's only used
    in one place.

--- director.php
+++ director.php
@@ -42,7 +42,16 @@
                         Stage Improv classes</a> for the details!
                     </p>
                 </div>
-                <?php include 'stan-sidebar.php' ?>
+                <div class="sidebar">
+                    <p>
+                        In 1998 Stan founded <a
+                        href="http://emptystageimprov.com/">The Empty
+                        Stage Theatre</a> in Los Angeles where he
+                        created numerous improv groups and directed
+                        over 1500 shows
+                        <img style="display:block" src="images/stanwells.26.jpg"/>
+                    </p>
+                </div>
                 <p>
                     Stan Wells began directing and teaching comedy
                     improvisation twenty-five years ago in Los
